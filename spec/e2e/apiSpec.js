55df1e0688192991f24d7a49b1a8c9c1ee646363
jorge jardines <gardenunez@gmail.com>
2016-08-23 01:46:39 +0200

    Add migrations and adjust test to run server

--- spec/e2e/apiSpec.js
+++ spec/e2e/apiSpec.js
@@ -1,25 +1,44 @@
 'use strict';
-var request = require('request'); 
+
+var request = require('request');
 var dbSession = require('../../src/backend/dbSession.js');
+var Server = require('../../src/backend/server.js').Server;
 var resetDatabase = require('../resetDatabase.js');
 var async = require('async');
 
-describe('The API', function(){
- it('should respond to a GET request at /api/keywords/', function (done) { 
-     var expected = {
-         "_items" : [
-             {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
-             {'id': 2, 'value': 'Onion', 'categoryID': 1},
-             {'id': 3, 'value': 'Knife', 'categoryID': 2}, 
+describe('The API', function () {
 
-         ]
-     };
-      async.series(
+  var server;
+
+  beforeEach(function (done) {
+    server = Server('8081');
+    server.listen(function (err) {
+      resetDatabase(dbSession, function() {
+        done(err);
+      });
+    });
+  });
+
+  afterEach(function (done) {
+    server.close(function () {
+      resetDatabase(dbSession, function() {
+        done();
+      });
+    });
+  });
+
+
+  it('should respond to a GET request at /api/keywords/', function (done) {
+    var expected = {
+      "_items": [
+        {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
+        {'id': 2, 'value': 'Onion', 'categoryID': 1},
+        {'id': 3, 'value': 'Knife', 'categoryID': 2}
+      ]
+    };
+
+    async.series(
       [
-        
-        function(callback){
-            resetDatabase(dbSession, callback);
-        },
 
         function(callback) {
           dbSession.insert(
@@ -48,7 +67,7 @@ describe('The API', function(){
         if (err) throw(err);
         request.get(
           {
-            'url': 'http://localhost:8080/api/keywords/',
+            'url': 'http://localhost:8081/api/keywords/',
             'json': true
           },
           function (err, res, body) {
@@ -57,6 +76,10 @@ describe('The API', function(){
             done();
           }
         );
-      } );
- });
+      }
+
+    );
+
+  });
 });
+
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,24 +1,8 @@
 'use strict';
 
-var Percolator = require('percolator').Percolator;
-var dbSession = require('../../src/backend/dbSession.js'); 
+var Server = require('./Server.js');
 
-var port = 8080;
-var server = Percolator({'port': port, 'autoLink': false});
-
-server.route('/api/keywords', {
-    GET: function(req, res){
-         dbSession.fetchAll('SELECT id, value, categoryID FROM keyword ORDER BY id', function(err, rows){
-             if(err){
-                 console.log(err);
-                 res.status.internalServerError(err);
-             }
-             else{
-                 res.collection(rows).send();
-             }
-         });
-    }
-});
+var server = Server('8080');
 
 server.listen(function(){
     console.log('Server started and listening on port', port); 
